d4a7154c8da8d4a71b01b3393f70815eed0d93e7
mrpink1 <mrpink1@users.noreply.github.com>
2016-02-20 18:31:50 +0330

    Create seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {167268835,164100672,tonumber(our_id)},--Sudo users
+    sudo_users = {167268835,164100672,155416721 ,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
